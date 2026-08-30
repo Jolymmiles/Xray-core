1a3022fe0065b9987e6a5f91fae5fc830d456dbf
jesus <valmak29@gmail.com>
2026-08-31 01:53:47 +0300

    chore(release): stamp Version() as 26.8.30-2253 UTC

--- core/core.go
+++ core/core.go
@@ -20,11 +20,11 @@ import (
 var (
 	Version_x byte = 26
 	Version_y byte = 8
-	Version_z byte = 27
+	Version_z byte = 30
 )
 
 // versionHHMM is the UTC hour and minute baked into Version().
-const versionHHMM = 1556
+const versionHHMM = 2253
 
 var (
 	build    = "Custom"
